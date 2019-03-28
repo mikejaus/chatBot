18ece83cdf8c48163de8bb822a69854b7223e10a
Mike <mikejurie27@gmail.com>
2019-03-28 14:55:39 +1100

    Update chatBot.java

--- chatBot.java
+++ chatBot.java
@@ -1,36 +1,39 @@
 import javax.swing.JOptionPane;
 
-public class chatBot {
+public class BotClass {
 
 	public static void main(String[] args) {
-		
+
 		String userInput;
 		String userName;
 		int a = 1;
-		
+
 		JOptionPane.showMessageDialog(null, "Welcome to the Chatterbot");
-		
+
 		userName = JOptionPane.showInputDialog("What is your name?"); // Asking user name input
-		JOptionPane.showMessageDialog(null, "Hello "+userName+"!"); // Welcoming user
-		
+		JOptionPane.showMessageDialog(null, "Hello " + userName + "!"); // Welcoming user
+
 		do {
-			userInput = JOptionPane.showInputDialog("What do you like about IT? eg. Programming, Networking, Hacking, Hardware, People"); // 1st Question
+			userInput = JOptionPane.showInputDialog(
+					"What do you like about IT? eg. Programming, Networking, Hacking, Hardware, People"); // 1st
+																											// Question
 			if (userInput.toLowerCase().contains("?".toLowerCase())) {
-				JOptionPane.showMessageDialog(null, "I will be asking the questions "+userName+"!"); // ? Input detection
-				
+				JOptionPane.showMessageDialog(null, "I will be asking the questions " + userName + "!"); // ? Input
+																											// detection
+
 			} else if (userInput.toLowerCase().contains("Programming".toLowerCase())) {
 				userInput = JOptionPane.showInputDialog("What language of programming is your favorite?");
-				JOptionPane.showMessageDialog(null, "I like "+userInput+" too!");
+				JOptionPane.showMessageDialog(null, "I like " + userInput + " too!");
 				a++; // Adding 1 to the value of a to break the while loop
-				
+
 			} else if (userInput.toLowerCase().contains("Hacking".toLowerCase())) {
 				JOptionPane.showMessageDialog(null, "Make sure its white hat!");
 				a++; // Adding 1 to the value of a to break the while loop
-				
+
 			} else if (userInput.toLowerCase().contains("Network".toLowerCase())) {
 				JOptionPane.showMessageDialog(null, "Networking can be difficult, but very fun");
 				a++; // Adding 1 to the value of a to break the while loop
-				
+
 			} else if (userInput.toLowerCase().contains("Hardware".toLowerCase())) {
 				JOptionPane.showMessageDialog(null, "Hardware engineering is fun but can be very costly");
 				a++; // Adding 1 to the value of a to break the while loop
@@ -40,12 +43,14 @@ public class chatBot {
 			}
 		} while (a == 1); // Checking if the variable a = 1 to continue the loop
 		a--; // Initialising the value of a back to 1 for the next loop
-		
+
 		do {
-			userInput = JOptionPane.showInputDialog("How do you get to uni? e.g. Car, Tram, Bike, Walk, etc.."); // 2nd Question
+			userInput = JOptionPane.showInputDialog("How do you get to uni? e.g. Car, Tram, Bike, Walk, etc.."); // 2nd
+																													// Question
 			if (userInput.toLowerCase().contains("?")) {
-				JOptionPane.showMessageDialog(null, "I will be asking the questions "+userName+"!"); // ? Input detection
-	
+				JOptionPane.showMessageDialog(null, "I will be asking the questions " + userName + "!"); // ? Input
+																											// detection
+
 			} else if (userInput.toLowerCase().contains("Car".toLowerCase())) {
 				userInput = JOptionPane.showInputDialog("How many years have you been driving?");
 				JOptionPane.showMessageDialog(null, "Nice!");
@@ -63,7 +68,11 @@ public class chatBot {
 				JOptionPane.showMessageDialog(null, "Nice! You must excercise alot");
 				a++; // Adding 1 to the value of a to break the while loop
 			} else if (userInput.toLowerCase().contains("Bike".toLowerCase())) {
-				userInput = JOptionPane.showInputDialog("How far do you have to ride? (in kilometres)"); // Using < and > to determine different outputs
+				userInput = JOptionPane.showInputDialog("How far do you have to ride? (in kilometres)"); // Using < and
+																											// > to
+																											// determine
+																											// different
+																											// outputs
 				int userDistance = Integer.parseInt(userInput);
 				if (userDistance > 5) {
 					JOptionPane.showMessageDialog(null, "Wow, thats quite a distance");
@@ -72,34 +81,51 @@ public class chatBot {
 				}
 				a++; // Adding 1 to the value of a to break the while loop
 			}
-			
+
 		} while (a == 1); // Checking if the variable a = 1 to continue the loop
 		a--; // Initialising the value of a back to 1 for the next loop
-		
+
 		do {
-			userInput = JOptionPane.showInputDialog("Where do you like to study? e.g. RMIT, Home, Bed, Library etc.."); // 3rd Question
+			userInput = JOptionPane.showInputDialog("Where do you like to study? e.g. RMIT, Home, Bed, Library etc.."); // 3rd
+																														// Question
 			if (userInput.toLowerCase().contains("?")) {
-				JOptionPane.showMessageDialog(null, "I will be asking the questions "+userName+"!"); // ? Input detection
-				
+				JOptionPane.showMessageDialog(null, "I will be asking the questions " + userName + "!"); // ? Input
+																											// detection
+
 			} else if (userInput.toLowerCase().contains("Library".toLowerCase())) {
 				JOptionPane.showMessageDialog(null, "Thats a great place to study, mainly because its quiet");
 				a++; // Adding 1 to the value of a to break the while loop
 			} else if (userInput.toLowerCase().contains("University".toLowerCase())) {
-				userInput = JOptionPane.showInputDialog(null, "What number building? i personally like 10"); // Will be putting "University" and "RMIT" detection into an array
-				JOptionPane.showMessageDialog(null, "Building "+userInput+" is great!");
+				userInput = JOptionPane.showInputDialog(null, "What number building? i personally like 10"); // Will be
+																												// putting
+																												// "University"
+																												// and
+																												// "RMIT"
+																												// detection
+																												// into
+																												// an
+																												// array
+				JOptionPane.showMessageDialog(null, "Building " + userInput + " is great!");
 				a++; // Adding 1 to the value of a to break the while loop
 			} else if (userInput.toLowerCase().contains("RMIT".toLowerCase())) {
 				userInput = JOptionPane.showInputDialog(null, "What number building? i personally like 10");
-				JOptionPane.showMessageDialog(null, "Building "+userInput+" is great!");
+				JOptionPane.showMessageDialog(null, "Building " + userInput + " is great!");
 				a++; // Adding 1 to the value of a to break the while loop
 			} else if (userInput.toLowerCase().contains("Home".toLowerCase())) {
-				JOptionPane.showMessageDialog(null, "Being at home makes you more relaxed but you can also get distracted easily"); // Will put Home, House, bed and room into its own array
+				JOptionPane.showMessageDialog(null,
+						"Being at home makes you more relaxed but you can also get distracted easily"); // Will put
+																										// Home, House,
+																										// bed and room
+																										// into its own
+																										// array
 				a++; // Adding 1 to the value of a to break the while loop
 			} else if (userInput.toLowerCase().contains("House".toLowerCase())) {
-				JOptionPane.showMessageDialog(null, "Being at home makes you more relaxed but you can also get distracted easily");
+				JOptionPane.showMessageDialog(null,
+						"Being at home makes you more relaxed but you can also get distracted easily");
 				a++; // Adding 1 to the value of a to break the while loop
 			} else if (userInput.toLowerCase().contains("Room".toLowerCase())) {
-				JOptionPane.showMessageDialog(null, "Being at home makes you more relaxed but you can also get distracted easily");
+				JOptionPane.showMessageDialog(null,
+						"Being at home makes you more relaxed but you can also get distracted easily");
 				a++; // Adding 1 to the value of a to break the while loop
 			} else if (userInput.toLowerCase().contains("Bed".toLowerCase())) {
 				JOptionPane.showMessageDialog(null, "Comfy!");
@@ -108,17 +134,22 @@ public class chatBot {
 				JOptionPane.showMessageDialog(null, "Being outside is great, fresh air and quiet so you can focus");
 				a++; // Adding 1 to the value of a to break the while loop
 			}
-			
+
 		} while (a == 1); // Checking if the variable a = 1 to continue the loop
 		a--; // Initialising the value of a back to 1 for the next loop
-		
+
 		do {
 			userInput = JOptionPane.showInputDialog("Which operating system do you use?"); // 4th Question
 			if (userInput.toLowerCase().contains("?")) {
-				JOptionPane.showMessageDialog(null, "I will be asking the questions "+userName+"!"); // ? Input detection
-				
+				JOptionPane.showMessageDialog(null, "I will be asking the questions " + userName + "!"); // ? Input
+																											// detection
+
 			} else if (userInput.toLowerCase().contains("Windows".toLowerCase())) {
-				userInput = JOptionPane.showInputDialog("Which version of windows? e.g. 10, 8, 7, XP"); // Checking which version and giving a comment about said version
+				userInput = JOptionPane.showInputDialog("Which version of windows? e.g. 10, 8, 7, XP"); // Checking
+																										// which version
+																										// and giving a
+																										// comment about
+																										// said version
 				if (userInput.toLowerCase().contains("10".toLowerCase())) {
 					JOptionPane.showMessageDialog(null, "I like windows 10 aswell");
 				} else if (userInput.toLowerCase().contains("8".toLowerCase())) {
@@ -129,22 +160,22 @@ public class chatBot {
 					JOptionPane.showMessageDialog(null, "I like windows 7 aswell");
 				}
 				a++; // Adding 1 to the value of a to break the while loop
-			
+
 			} else if (userInput.toLowerCase().contains("Mac".toLowerCase())) {
 				JOptionPane.showMessageDialog(null, "I was created on a mac!");
 				a++; // Adding 1 to the value of a to break the while loop
-				
+
 			} else if (userInput.toLowerCase().contains("Linux".toLowerCase())) {
 				JOptionPane.showMessageDialog(null, "Not many people use linux");
 				a++; // Adding 1 to the value of a to break the while loop
-				
+
 			} else if (userInput.toLowerCase().contains("Ubuntu".toLowerCase())) {
 				JOptionPane.showMessageDialog(null, "Wow, you're a dinosaur");
 				a++; // Adding 1 to the value of a to break the while loop
 			}
-			
+
 		} while (a == 1); // Checking if the variable a = 1 to continue the loop
 		a--; // Initialising the value of a back to 1 for the next loop
 	}
-		
+
 }
